7f2084926dcda7836541fd601392c46d675e9a08
Stephan Beyer <s-beyer@gmx.net>
2016-01-24 16:08:34 +0100

    Add util/check-doxygen-errors.sh
    It checks for doxygen errors and warnings.
    
    Travis CI now uses it to perform these checks.

---
+++ util/check-doxygen-errors.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Run doxygen and fail if there are warnings or errors
+
+STDOUT="dox-stdout-$$"
+STDERR="dox-stderr-$$"
+
+make doc >$STDOUT 2>$STDERR
+cleanup() {
+	rm -f "$STDOUT" "$STDERR"
+}
+trap cleanup 1 2 3 6 15
+
+cat $STDOUT
+
+ret=0
+
+if [ -s $STDERR ]
+then
+	echo
+	echo "== Doxygen warnings/errors =="
+	cat "$STDERR"
+	ret=`wc -l "$STDERR"`
+fi
+
+cleanup
+exit $ret
