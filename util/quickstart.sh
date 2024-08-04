52bb2103cb2f700b0cb35c1177cbb488142ec9e5
Stephan Beyer <s-beyer@gmx.net>
2024-08-04 02:33:36 +0200

    Generate compile_commands.json via quickstart

--- util/quickstart.sh
+++ util/quickstart.sh
@@ -7,7 +7,10 @@ CMAKE_SOLVERS="$(for solver in $SOLVERS ; do echo -n " -DUSE_$solver=ON" ; done)
 
 git submodule update --init --recursive &&
 make -C solvers &&
-cmake -B build $CMAKE_SOLVERS . &&
-cd build &&
-make &&
-ctest
+cmake -B build $CMAKE_SOLVERS -DCMAKE_EXPORT_COMPILE_COMMANDS=ON . || exit
+
+( cd build &&
+  make &&
+  ctest )
+
+test -e compile_commands.json || ln -s build/compile_commands.json
